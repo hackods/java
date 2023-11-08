cd923583ca7426ce79135fc69f310efa69b3a941
Hackods <hcodester@gmail.com>
2023-11-08 15:49:10 +0530

    commit

--- searching/linearsearch.java
+++ searching/linearsearch.java
@@ -2,6 +2,7 @@ package searching;
 import java.util.Scanner;
 public class linearsearch {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        sc.close();
     }
 }
