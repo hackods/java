00b4f1e27f8a61acbf78087e89b86397393f6dff
Hackods <hcodester@gmail.com>
2023-11-13 15:10:50 +0530

    commit

--- searching/linearsearch.java
+++ searching/linearsearch.java
@@ -4,9 +4,31 @@ public class linearsearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[10];
-        for (int i=0; i<10; i++)
+        int i, c=0, n;
+        for (i=0; i<10; i++)
         {
             arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter number to be searched: ");
+        n = sc.nextInt();
+        for (i=0; i<10; i++)
+        {
+            if (a[i]==n)
+            {
+                c=1;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        if (c==1)
+        {
+            System.out.println(n+ "is present in position " +i+ " of the array.");
+        }
+        else
+        {
+            System.out.println(n+ "is not present in the array.");
         }
         sc.close();
     }
