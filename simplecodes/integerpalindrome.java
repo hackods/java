26bafb184de923ff393d447a2bf198b8e802b054
Hackods <hcodester@gmail.com>
2023-11-13 16:10:53 +0530

    commit

---
+++ simplecodes/integerpalindrome.java
@@ -0,0 +1,7 @@
+package simplecodes;
+import java.util.Scanner;
+public class integerpalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
