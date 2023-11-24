3336838fcd90851a746a15525123a842018809fe
Hackods <hcodester@gmail.com>
2023-11-24 09:31:56 +0530

    commit

---
+++ overloading/dudeneypattern.java
@@ -0,0 +1,62 @@
+package overloading;
+import java.util.Scanner;
+public class dudeneypattern{
+    public void print(){
+        int i, j, k=1;
+        for (i = 1; i<4; i++)
+        {
+            for (j = 1; j<i; j++)
+            {
+                System.out.print(k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+    public boolean print(int a){
+        int p, s=0, n = a;
+        while (n>0)
+        {
+            p = n%10;
+            s = s+p;
+            n = n/10;
+        }
+        if (s==a)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        dudeneypattern ob = new dudeneypattern();
+        System.out.println("1. Pattern."
+                            +"2. Dudeney Check.");
+        System.out.print("Enter Choice: ");
+        int c = sc.nextInt();
+        switch (c) {
+            case 1:
+                ob.print();
+                break;
+            case 2:
+                System.out.print("Enter a number: ");
+                int a = sc.nextInt();
+                boolean x = ob.print(a);
+                if(x)
+                {
+                    System.out.println("Dudeney Number.");
+                }
+                else
+                {
+                    System.out.println("Not Dudeney Number.");
+                }
+            default:
+                System.out.println("Wrong Choice!");
+                System.exit(0);
+        }
+        sc.close();
+    }
+}
