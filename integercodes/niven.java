7bc882c8c817879d62fe5dbd6ac47d2b37cf4a45
Hackods <hcodester@gmail.com>
2023-12-26 14:29:00 +0530

    commit

---
+++ integercodes/niven.java
@@ -0,0 +1,25 @@
+package integercodes;
+import java.util.Scanner;
+public class niven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int m=n;
+        int s=0, p;
+        while (n>0)
+        {
+            p = n%10;
+            s = s+p;
+            n = n/10;
+        }
+        if (n%s==0)
+        {
+            System.out.println("Niven Number.");
+        }
+        else
+        {
+            System.out.println("Not Niven Number.");
+        }
+    }
+}
