b581744e3e12ad51dcd784017a2c3e8cb378de97
Hackods <hcodester@gmail.com>
2023-12-26 16:31:15 +0530

    commit

--- patterns/Pattern3.java
+++ patterns/Pattern3.java
@@ -1,14 +1,14 @@
 package patterns;
 import java.util.Scanner;
-public class Pattern2 {
+public class Pattern3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
         sc.close();
-        for (int i=n; i>=1; i--)
+        for (int i = 1; i <= n; i++)
         {
-            for (int j=1; j<=i; j++)
+            for (int j = 1; j <= i; j++)
             {
                 System.out.print(j+" ");
             }
--- patterns/Pattern4.java
+++ patterns/Pattern4.java
@@ -1,16 +1,14 @@
 package patterns;
 import java.util.Scanner;
-public class Pattern1 {
+public class Pattern4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.err.println("Enter number of rows: ");
+        System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
-        System.out.println("Enter number of columns:");
-        int m = sc.nextInt();
         sc.close();
-        for (int i = 1; i <= n; i++)
+        for (int i = n; i >= 1; i--)
         {
-            for (int j = 1; j <= m; j++)
+            for (int j = 1; j <= i; j++)
             {
                 System.out.print(j+" ");
             }
