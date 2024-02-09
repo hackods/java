2eeeb7612e0e8a5fdcf225217a4f55661dd7cc4e
Hackods <hcodester@gmail.com>
2024-02-09 21:26:50 +0530

    commit

---
+++ stringcodes/sumDigits.java
@@ -0,0 +1,26 @@
+package stringcodes;
+import java.util.Scanner;
+public class sumDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+        sc.close();
+        int l = s.length();
+        char c;
+        int sum=0;
+        for (int i = 0; i<l; i++)
+        {
+            c = s.charAt(i);
+            if (c>='0' && c<='9')
+            {
+                sum = sum + (int)c-48;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        System.out.println("Sum of digits: "+sum);
+    }
+}
